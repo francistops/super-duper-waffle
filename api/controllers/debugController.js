f373ded9a2820fa22d61085347b715143831ca35
francistops <65456051+francistops@users.noreply.github.com>
2025-06-29 14:01:14 -0400

    fix debug controller

--- api/controllers/debugController.js
+++ api/controllers/debugController.js
@@ -1,7 +1,6 @@
-import { fetchUsers, fetchTokens } from "../models/debugModel.js";
-
 import { catchMsg } from "../lib/utils.js";
-// catchMsg(name, errorHttp, errorObj, res, result)
+
+import {fetchUsers, fetchTokens} from "../models/debugModel.js"
 
 const UNKNOWN_ERROR = {
 	message: "Unknown error",
@@ -34,27 +33,6 @@ export async function getTokens(req, res) {
 		};
 	} catch (error) {
 		catchMsg("debug getTokens");
-	}
-	res.formatView(result);
-}
-
-export async function registerUser(req, res) {
-	console.log("---in userController registerUser---");
-	let result = UNKNOWN_ERROR;
-	const newUser = req.body;
-	// console.log(newUser);
-	try {
-		const createdUser = await insertUser(newUser);
-		console.log("after model", createdUser);
-		result = {
-			message: "Success",
-			errorCode: 0,
-		};
-	} catch (error) {
-		console.error("Error inserting user:", error);
-		res.status(500);
-		result.message = `Error inserting user`;
-		result.errorCode = 1002;
 	}
 	res.formatView(result);
 }
