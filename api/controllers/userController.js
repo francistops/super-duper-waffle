76e7e77418c87ebf9c78deecf4e6f961e90053fd
amelieroussin <amelie.roussin@gmail.com>
2025-07-15 17:36:17 -0400

    juste un bug de hier soir, je pull le main là

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -18,14 +18,11 @@ const UNKNOWN_ERROR = {
 	errorCode: 9999,
 };
 
-<<<<<<< HEAD
 // already in debugCtrl same reason as the one for the model
 // unleast you need it to be display or internal feature
 // it should be in debug i believe
 // export async function getAllUsers(req, res) {
 
-=======
->>>>>>> 28127b2 (Sorry j'étais sur le main, check dans readme pour voir ce qui fonctionne, je viens de penser que dans appointment controller il faut que je gère que c'est un token de client qui peut add ou modify un appointment, je ne l'ai pas fais encore)
 export async function getUsersByRole(req, res) {
 	let result = makeError();
 	const role = req.body.role;
